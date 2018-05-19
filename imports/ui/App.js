ea0d7866b5b2a8faf965957e7f742ad313960494
sergeialimov <sv.alimov@yandex.ru>
2018-05-19 12:47:58 +0300

    Storing temporary UI data in component state

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,7 +8,15 @@ import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
-	handleSubmi2t(event) {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+	handleSubmit(event) {
 		event.preventDefault();
 
 		// Find the text field via the React ref
@@ -19,12 +27,22 @@ class App extends Component {
 			createdAt: new Date(),
 		});
 
-		// Clear forums
+		// Clear form
 		ReactDOM.findDOMNode(this.refs.textInput).value = '';
 	}
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -34,7 +52,18 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
-					<form className="new-task" onSubmit={this.handleSubmi2t.bind(this)} >
+
+          <label className="hide-completed">
+          <input
+            type="checkbox"
+            readOnly
+            checked={this.state.hideCompleted}
+            onClick={this.toggleHideCompleted.bind(this)}
+          />
+          Hide Completed Tasks
+          </label>
+
+					<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
 						<input
 							type="text"
 							ref="textInput"
