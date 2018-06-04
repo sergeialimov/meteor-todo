8e434fa66aacc9d81e59b495e634787c4529c3dc
sergeialimov <sv.alimov@yandex.ru>
2018-06-04 22:45:02 +0300

    Add publication for tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,13 @@ import { check } from 'meteor/check'
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
@@ -22,7 +29,7 @@ Meteor.methods({
   },
   'tasks.remove'(taskId) {
     check(taskId, String);
-    
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
