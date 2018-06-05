dacbd982c981e0eb4f9e5baef23a8291f179c5bd
sergeialimov <sv.alimov@yandex.ru>
2018-06-05 09:29:47 +0300

    Only publish tasks the current user can see

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,12 +4,15 @@ import { check } from 'meteor/check'
 
 export const Tasks = new Mongo.Collection('tasks');
 
-if (Meteor.isServer) {
-  // This code only runs on the server
-  Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
-  });
-}
+// Only publish tasks that are public or belong to the current user
+ Meteor.publish('tasks', function tasksPublication() {
+   return Tasks.find({
+     $or: [
+       { private: { $ne: true } },
+       { owner: this.userId },
+     ],
+   });
+ });
 
 Meteor.methods({
   'tasks.insert'(text) {
